4479962baf61d2ccaa198b61e7e47fbc798fcb3e
hkoivisto <71294946+hkoivisto@users.noreply.github.com>
2020-11-01 16:36:26 -0600

    Add files via upload

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -12,6 +12,7 @@ ON (e.emp_no = ts.emp_no)
 WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
 ORDER BY e.emp_no;
 
+
 -- Use Dictinct with Orderby to remove duplicate rows
 SELECT DISTINCT ON (rt.emp_no) rt.emp_no,
 rt.first_name,
@@ -27,3 +28,22 @@ INTO retiring_titles
 FROM unique_titles as ut
 GROUP BY ut.title
 ORDER BY count DESC;
+
+--Create mentor eligibility table
+SELECT DISTINCT ON(e.emp_no) e.emp_no,
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+ti.title
+INTO mentorship_eligibility
+FROM employees as e
+JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE de.to_date = ('9999-01-01')
+AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY e.emp_no, ti.to_date DESC;
+
